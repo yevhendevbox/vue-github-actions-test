8b98421bd7f56893f14f2b80f6645f2cec814bb7
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-03-06 11:03:02 +0200

    feat: update unit tests

---
+++ tests/unit/components/WelcomeItem.spec.ts
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils'
+import WelcomeItem from '@/components/WelcomeItem.vue'
+import { describe, it, expect } from 'vitest'
+
+describe('WelcomeItem', () => {
+  const wrapper = mount(WelcomeItem, {
+    slots: {
+      default: '<div>Hello world</div>',
+      heading: '<h3>Test Heading</h3>'
+    }
+  })
+
+  it('renders properly', () => {
+    expect(wrapper.exists()).toBeTruthy()
+  })
+  it('should render slots content properly', () => {
+    expect(wrapper.text()).toContain('Hello world')
+    expect(wrapper.text()).toContain('Test Heading')
+  })
+})
